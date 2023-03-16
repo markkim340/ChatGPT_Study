f3cb548f3794211a5b9cc9c1fd181ef27950fef7
Mark Kim <mark340@naver.com>
2023-03-17 02:12:12 +0900

    Refactor

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('*')
+  getIndex(@Req() req: Request, @Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public/index.html'));
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const PORT = process.env.PORT;
   await app.listen(PORT, () => console.log(`🔰Listening on PORT:${PORT}`));
